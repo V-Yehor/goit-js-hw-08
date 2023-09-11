3c025d8d3a49a0ffb0818aa11d79d618cf85a7ea
V-Yehor <es.valkoff@gmail.com>
2023-09-11 20:17:53 +0300

    Fourth commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
 
+const LS_KEY = 'videoplayer-current-time';
+const player = new Player('vimeo-player', {
+    id: 236203659,
+    width: 640
+});
+player.on('timeupdate', function (event) {
+    const currentPlayerTime = event.seconds;
+    localStorage.setItem(LS_KEY, JSON.stringify(currentPlayerTime));
+});
+
+const setupPlayerTime = localStorage.getItem(LS_KEY);
+// console.log(setupPlayerTime);
+
+player.setCurrentTime(setupPlayerTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
