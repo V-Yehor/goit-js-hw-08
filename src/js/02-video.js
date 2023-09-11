97389d32c7fba7eeef09e1290ceca4df6da70d72
V-Yehor <es.valkoff@gmail.com>
2023-09-11 23:06:17 +0300

    Fifth commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,20 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// console.log(throttle);
 
 const LS_KEY = 'videoplayer-current-time';
 const player = new Player('vimeo-player', {
     id: 236203659,
     width: 640
 });
-player.on('timeupdate', function (event) {
+const timeUpdateFn = function (event) {
     const currentPlayerTime = event.seconds;
-    localStorage.setItem(LS_KEY, JSON.stringify(currentPlayerTime));
-});
+    localStorage.setItem(LS_KEY, currentPlayerTime);
+}
+
+player.on('timeupdate', throttle(timeUpdateFn, 1000));
+
+
 
 const setupPlayerTime = localStorage.getItem(LS_KEY);
 // console.log(setupPlayerTime);
